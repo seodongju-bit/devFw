37230a5560944ff41a6dc36972af5148b5c2cba6
jaeho <jaeho7224@naver.com>
2019-11-29 10:00:06 +0900

    assad

--- devFw/src/main/java/project/C/P003/dao/C_P003DAO.java
+++ devFw/src/main/java/project/C/P003/dao/C_P003DAO.java
@@ -4,7 +4,13 @@ package project.C.P003.dao;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.dao.DataAccessException;
+import org.springframework.web.servlet.ModelAndView;
+
+import project.C.P001.vo.PagingVO;
 
 
 public interface C_P003DAO {
@@ -17,6 +23,12 @@ public interface C_P003DAO {
 
 	public List noticeDetail2(Map<String, Object> dataMap2) throws DataAccessException;
 	
+	public List totalnoticeList(PagingVO vo) throws DataAccessException;
+	
 	public int countBoard();
 	
+	
+
+	
+  
 }
